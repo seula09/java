450beb80cdedd290909e676ee52db8d40eeecb80
yxnuoes <yoonseula09@gmail.com>
2025-09-24 14:46:56 +0900

    250924

---
+++ chap07/src/sec01/exam03/Computer.java
@@ -0,0 +1,12 @@
+package sec01.exam03;
+
+public class Computer extends Calculator{
+	
+	// 메소드 재정의 
+	@Override
+	double areaCircle(double r) {
+		System.out.println("Computer 객체의 areaCircle() 실행");
+			return  Math.PI * r * r;
+	}
+
+}
